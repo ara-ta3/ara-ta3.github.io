30d36e449482324bebaf0addd1e0b82a091d263f
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-22 23:01:48 +0900

    Add url

--- src/pages/CatCalorie.tsx
+++ src/pages/CatCalorie.tsx
@@ -94,50 +94,41 @@ const Feed: React.FC<{ der: number }> = (props: { der: number }) => {
       id: 1,
       name: "ピュリナワン チキン",
       kcalPer100: 413,
+      url: "https://nestle.jp/brand/one/cat/lineup/grainfree1/",
     },
   ];
 
-  const FeedData: React.FC = () => {
-    return (
-      <>
-        {feeds.map((f) => {
-          return (
-            <div key={f.id}>
-              <div className="px-4 sm:px-0">
-                <h3 className="text-base/7 font-semibold text-gray-900">
-                  {f.name}
-                </h3>
-              </div>
-              <div className="mt-6 border-t border-gray-100">
-                <dl className="divide-y divide-gray-100">
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm/6 font-medium text-gray-900">
-                      カロリー
-                    </dt>
-                    <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                      {f.kcalPer100}
-                    </dd>
-                  </div>
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm/6 font-medium text-gray-900">
-                      必要グラム数
-                    </dt>
-                    <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                      {Math.round(props.der / f.kcalPer100 * 100)}
-                    </dd>
-                  </div>
-                </dl>
-              </div>
-            </div>
-          );
-        })}
-      </>
-    );
-  };
-
   return (
     <div className="p-4 bg-white shadow rounded">
-      <FeedData />
+      {feeds.map((f) => {
+        return (
+          <div key={f.id}>
+            <div className="px-4 sm:px-0">
+              <a href={f.url} target="_blank">{f.name}</a>
+            </div>
+            <div className="mt-6 border-t border-gray-100">
+              <dl className="divide-y divide-gray-100">
+                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                  <dt className="text-sm/6 font-medium text-gray-900">
+                    カロリー
+                  </dt>
+                  <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                    {f.kcalPer100}
+                  </dd>
+                </div>
+                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                  <dt className="text-sm/6 font-medium text-gray-900">
+                    必要グラム数
+                  </dt>
+                  <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                    {Math.round(props.der / f.kcalPer100 * 100)}
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
