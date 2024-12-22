0d290adace3682ae6d1396950decf7d15df483f9
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-22 21:26:22 +0900

    Add 餌

--- src/pages/CatCalorie.tsx
+++ src/pages/CatCalorie.tsx
@@ -68,6 +68,60 @@ const Results: React.FC<{ results: { rer: number; der: number } | null }> = (
   );
 };
 
+const Feed: React.FC<{ der: number }> = (props: { der: number }) => {
+  const feeds = [
+    {
+      id: 1,
+      name: "ピュリナワン チキン",
+      kcalPer100: 413,
+    },
+  ];
+
+  const FeedData: React.FC = () => {
+    return (
+      <>
+        {feeds.map((f) => {
+          return (
+            <div key={f.id}>
+              <div className="px-4 sm:px-0">
+                <h3 className="text-base/7 font-semibold text-gray-900">
+                  {f.name}
+                </h3>
+              </div>
+              <div className="mt-6 border-t border-gray-100">
+                <dl className="divide-y divide-gray-100">
+                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt className="text-sm/6 font-medium text-gray-900">
+                      カロリー
+                    </dt>
+                    <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                      {f.kcalPer100}
+                    </dd>
+                  </div>
+                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt className="text-sm/6 font-medium text-gray-900">
+                      必要グラム数
+                    </dt>
+                    <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                      {Math.round(props.der / f.kcalPer100 * 100)}
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+
+  return (
+    <div className="p-4 bg-white shadow rounded">
+      <FeedData />
+    </div>
+  );
+};
+
 const CatCalorie: React.FC = () => {
   const [results, setResults] = useState<{ rer: number; der: number } | null>(
     null,
@@ -81,6 +135,7 @@ const CatCalorie: React.FC = () => {
           <h1 className="text-2xl font-bold mb-4">猫のカロリー計算</h1>
           <CalculatorForm setResults={setResults} />
           <Results results={results} />
+          <Feed der={results?.der ?? 0} />
         </div>
       </main>
     </>
