4c06ac08e4ecba3023056b12e90c8fc44958a64a
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-22 23:03:29 +0900

    Add unit

--- src/pages/CatCalorie.tsx
+++ src/pages/CatCalorie.tsx
@@ -110,7 +110,7 @@ const Feed: React.FC<{ der: number }> = (props: { der: number }) => {
               <dl className="divide-y divide-gray-100">
                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                   <dt className="text-sm/6 font-medium text-gray-900">
-                    カロリー
+                    カロリー(kcal/100g)
                   </dt>
                   <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
                     {f.kcalPer100}
