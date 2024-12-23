fb725cb02402f3743524c17e25db83e5d58321bb
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-23 16:58:57 +0900

    Add ref and simple rer

--- src/components/cats/CalorieCalculator.tsx
+++ src/components/cats/CalorieCalculator.tsx
@@ -17,8 +17,10 @@ const multipliers = [{
 
 const CalorieCalculator: React.FC<
   {
-    setResults: (results: { rer: number; der: number } | null) => void;
-    results: { rer: number; der: number } | null;
+    setResults: (
+      results: { rer: number; simpleRER: number; der: number } | null,
+    ) => void;
+    results: { rer: number; simpleRER: number; der: number } | null;
   }
 > = ({ setResults, results }) => {
   const [weight, setWeight] = useState<string>("");
@@ -39,8 +41,9 @@ const CalorieCalculator: React.FC<
     if (!isNaN(parsedWeight) && parsedWeight > 0 && multiplier > 0) {
       const r = new DomainCatCalorie(parsedWeight);
       const rer = r.calculateRER();
+      const simpleRER = r.calculateSimpleRER();
       const der = r.calculateDER(multiplier);
-      setResults({ rer, der });
+      setResults({ rer, simpleRER, der });
     } else {
       setResults(null);
     }
@@ -105,10 +108,67 @@ const CalorieCalculator: React.FC<
         </div>
         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
           <dt className="text-sm/6 font-medium text-gray-900">
+            <p>簡易版RER = 体重 × 30 + 70</p>
+            <p className="text-xs text-gray-400">
+              計算結果のみで後の計算には使いません
+            </p>
+          </dt>
+          <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+            {(results?.simpleRER ?? 0).toFixed(2) ?? 0} kcal/day
+          </dd>
+        </div>
+        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt className="text-sm/6 font-medium text-gray-900">
             DER = RER × 係数
           </dt>
           <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
             {(results?.der ?? 0).toFixed(2)} kcal/day
+          </dd>
+        </div>
+        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt className="text-sm/6 font-medium text-gray-900">
+            参考
+          </dt>
+          <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+            <ul className="max-w-md space-y-1 text-gray-500 list-none list-inside dark:text-gray-400">
+              <li>
+                <a
+                  className="hover:underline text-blue-600"
+                  href="https://www.env.go.jp/nature/dobutsu/aigo/2_data/pamph/petfood_guide_1808/pdf/6.pdf"
+                  target="_blank"
+                >
+                  環境省 飼い主のためのペットフード・ガイドライン
+                </a>
+              </li>
+
+              <li>
+                <a
+                  className="hover:underline text-blue-600"
+                  href="https://www.anicom-sompo.co.jp/nekonoshiori/8965.html#i-2"
+                  target="_blank"
+                >
+                  【獣医師監修】猫にとって必要なカロリーとは？計算の仕方は？
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:underline text-blue-600"
+                  href="https://www.youtube.com/watch?v=i-R76COsVbU"
+                  target="_blank"
+                >
+                  【猫のご飯の量】獣医さんが使う猫のご飯の量の計算方法～フードの裏だと多すぎ！？～
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:underline text-blue-600"
+                  href="https://www.axa-direct.co.jp/pet/pet_cat/consultation/content/08.html"
+                  target="_blank"
+                >
+                  エサの適正量｜よくあるご相談｜猫のお悩み相談室｜アクサダイレクトのペット保険
+                </a>
+              </li>
+            </ul>
           </dd>
         </div>
       </dl>
--- src/domains/Cat.ts
+++ src/domains/Cat.ts
@@ -5,10 +5,14 @@ export class CatCalorie {
         this.weight = weight;
     }
 
-
     calculateRER(): number {
         return 70 * Math.pow(this.weight, 0.75);
     }
+
+    calculateSimpleRER(): number {
+        return this.weight * 30 + 70;
+    }
+
 
     calculateDER(multiplier: number): number {
         return this.calculateRER() * multiplier;
