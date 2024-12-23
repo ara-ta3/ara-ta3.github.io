f6486697450b00c716dd64ab4fb0e6d8bb739eeb
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-24 08:36:28 +0900

    Move ssg

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,20 +1,18 @@
 import React from "react";
-//import { useNavigate } from "react-router-dom";
 
 const Tools: React.FC = () => {
-  //  const navigate = useNavigate();
   const tools = [
     {
       id: 1,
       name: "24時間スケジューラー",
       description: "1日のスケジュールを入力し、グラフで生成します。",
-      //      navigate: () => navigate("/schedules"),
+      url: "/schedule",
     },
     {
       id: 2,
       name: "猫のカロリー計算",
       description: "RER/DERの計算をします。",
-      //      navigate: () => navigate("/cat/calorie"),
+      url: "/cat/calorie",
     },
   ];
   return (
@@ -23,14 +21,15 @@ const Tools: React.FC = () => {
         <h2 className="text-3xl font-bold mb-6 text-center">Tools</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {tools.map((tool) => (
-            <div
-              key={tool.id}
-              className="bg-white shadow-md rounded-lg p-6 cursor-pointer hover:bg-gray-100 transition"
-              //              onClick={tool.navigate}
-            >
-              <h3 className="text-xl font-semibold mb-2">{tool.name}</h3>
-              <p className="text-gray-600">{tool.description}</p>
-            </div>
+            <a href={tool.url}>
+              <div
+                key={tool.id}
+                className="bg-white shadow-md rounded-lg p-6 cursor-pointer hover:bg-gray-100 transition"
+              >
+                <h3 className="text-xl font-semibold mb-2">{tool.name}</h3>
+                <p className="text-gray-600">{tool.description}</p>
+              </div>
+            </a>
           ))}
         </div>
       </div>
--- src/pages/+Page.tsx
+++ src/pages/+Page.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 
-import Header from "../components/Header";
 import Tools from "../components/Tools";
 
 const Home: React.FC = () => {
   return (
     <>
-      <Header />
-      <main>
-        <Tools />
-      </main>
+      <Tools />
     </>
   );
 };
