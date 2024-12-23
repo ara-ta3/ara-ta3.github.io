95fef8295b8f7dc3626356c716fa0e1abf91ea1b
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-24 08:48:06 +0900

    Add cat page

---
+++ src/pages/cat/calorie/+Page.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Header from "../../../components/Header.tsx";
+import Caution from "../../../components/cats/Caution.tsx";
+import CalorieCalculator from "../../../components/cats/CalorieCalculator.tsx";
+
+import DryFoods from "../../../components/cats/DryFoods.tsx";
+
+const CatCalorie: React.FC = () => {
+  const [results, setResults] = useState<{ rer: number; der: number } | null>(
+    null,
+  );
+
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container mx-auto p-4">
+          <h1 className="text-2xl font-bold my-4">猫のカロリー計算</h1>
+          <Caution />
+          <CalorieCalculator results={results} setResults={setResults} />
+          <h1 className="text-2xl font-bold my-4 ">ドライフード</h1>
+          <DryFoods der={results?.der ?? 0} />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default CatCalorie;
---
+++ src/pages/cat/calorie/+config.ts
@@ -0,0 +1,4 @@
+export default {
+    title: '猫のカロリー計算|ara-ta3の物置',
+    description: '猫の餌のカロリー計算と市販の餌をどれくらいあげると良いかの計算をします。',
+};
