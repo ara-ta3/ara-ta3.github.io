f606d66ee1bf4e9f8058d721ba7991b35da797e6
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-24 08:49:48 +0900

    Move

---
+++ src/pages/schedule/+Page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { HourlyPieChart } from "../../components/Schedule";
+
+const Schedules: React.FC = () => {
+  return (
+    <>
+      <HourlyPieChart />
+    </>
+  );
+};
+
+export default Schedules;
