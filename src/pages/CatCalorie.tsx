9b3585a571039001cbd48a49b9786cbb14337bdc
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-23 14:23:09 +0900

    Impl some

--- src/pages/CatCalorie.tsx
+++ src/pages/CatCalorie.tsx
@@ -2,6 +2,22 @@ import React, { useEffect, useState } from "react";
 import { CatCalorie as DomainCatCalorie } from "../domains/Cat.ts";
 import Header from "../components/Header.tsx";
 
+const Caution: React.FC = () => {
+  return (
+    <>
+      <h2 className="text-1xl font-extrabold dark:text-white">
+        注意事項
+      </h2>
+      <p className="my-4 text-sm text-gray-500">
+        本サイトの計算結果は参考情報であり、猫の健康状態や栄養管理を保証するものではありません。
+      </p>
+      <p className="my-4 text-sm text-gray-500">
+        判断は飼い主様の責任でお願いいたします。必ずかかりつけの獣医師に相談の上でご利用ください。
+      </p>
+    </>
+  );
+};
+
 const CalculatorForm: React.FC<
   {
     setResults: (results: { rer: number; der: number } | null) => void;
@@ -34,19 +50,34 @@ const CalculatorForm: React.FC<
   }, [weight, multiplier, setResults]);
 
   const multipliers = [{
-    name: "維持",
+    name: "非活動的・肥満傾向",
     value: 1,
   }, {
-    name: "活発",
+    name: "減量が必要",
+    value: 0.8,
+  }, {
+    name: "少し増量が必要",
     value: 1.2,
   }, {
-    name: "成長",
+    name: "増量が必要",
     value: 1.4,
   }].map((m) => {
     return (
-      <option key={m.name} value={m.value}>
-        {`${m.name}(係数: ${m.value})`}
-      </option>
+      <div>
+        <label key={m.name}>
+          <input
+            className="mx-2"
+            type="radio"
+            value={m.value}
+            checked={m.value === multiplier}
+            onChange={(e) => {
+              localStorage.setItem("cat.multiplier", e.target.value);
+              setMultiplier(Number(e.target.value));
+            }}
+          />
+          {m.name}({m.value})
+        </label>
+      </div>
     );
   });
   return (
@@ -71,25 +102,18 @@ const CalculatorForm: React.FC<
         </div>
         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
           <dt className="text-sm/6 font-medium text-gray-900">
-            活動レベル
+            係数
           </dt>
           <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-            <select
-              value={multiplier}
-              onChange={(e) => {
-                localStorage.setItem("cat.multiplier", e.target.value);
-                setMultiplier(Number(e.target.value));
-              }}
-              className="w-full border rounded p-2 mt-1"
-            >
+            <div>
               {multipliers}
-            </select>
+            </div>
           </dd>
         </div>
 
         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
           <dt className="text-sm/6 font-medium text-gray-900">
-            RER
+            RER = 70 × (体重) ^ (3/4)
           </dt>
           <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
             {(results?.rer ?? 0).toFixed(2) ?? 0} kcal/day
@@ -97,7 +121,7 @@ const CalculatorForm: React.FC<
         </div>
         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
           <dt className="text-sm/6 font-medium text-gray-900">
-            DER
+            DER = RER × 係数
           </dt>
           <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
             {(results?.der ?? 0).toFixed(2)} kcal/day
@@ -164,8 +188,9 @@ const CatCalorie: React.FC = () => {
       <main>
         <div className="container mx-auto p-4">
           <h1 className="text-2xl font-bold my-4">猫のカロリー計算</h1>
+          <Caution />
           <CalculatorForm results={results} setResults={setResults} />
-          <h1 className="text-2xl font-bold my-4 ">餌</h1>
+          <h1 className="text-2xl font-bold my-4 ">ドライフード</h1>
           <Feed der={results?.der ?? 0} />
         </div>
       </main>
