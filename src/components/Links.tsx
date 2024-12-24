5643c409ea3c9ed7c5654506f8949512714a2ba6
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-25 00:48:23 +0900

    Add links

---
+++ src/components/Links.tsx
@@ -0,0 +1,44 @@
+import { Card } from "flowbite-react";
+import React from "react";
+import { useHref, useNavigate } from "react-router-dom";
+
+const Tools: React.FC = () => {
+  const links = [
+    {
+      name: "X(Twitter)",
+      href: "https://x.com/ara_ta3",
+    },
+    {
+      name: "GitHub",
+      href: "https://github.com/ara-ta3",
+    },
+    {
+      name: "zenn.dev",
+      href: "https://zenn.dev/ara_ta3",
+    },
+    {
+      name: "はてなブログ",
+      href: "https://arata.hatenadiary.com/",
+    },
+  ];
+  return (
+    <section id="links" className="py-12 bg-gray-50">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold mb-4 text-center">Links</h2>
+        <div className="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-4">
+          {links.map((link) => (
+            <a href={link.href} target="_blank">
+              <Card key={link.name} className="w-full">
+                <h5 className="text-base font-bold text-gray-900">
+                  {link.name}
+                </h5>
+              </Card>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tools;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,31 @@ import React from "react";
 
 import Header from "../components/Header";
 import Tools from "../components/Tools";
+import Links from "../components/Links";
+
+const PortfolioTop: React.FC = () => {
+  return (
+    <div className="h-[25vh] flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-gray-800 underline">
+          <a href="https://x.com/ara_ta3" target="_blank">
+            ara-ta3
+          </a>
+        </h1>
+        <p className="text-lg text-gray-600 mt-4">portfolio</p>
+      </div>
+    </div>
+  );
+};
 
 const Home: React.FC = () => {
   return (
     <>
       <Header />
       <main>
+        <PortfolioTop />
         <Tools />
+        <Links />
       </main>
     </>
   );
