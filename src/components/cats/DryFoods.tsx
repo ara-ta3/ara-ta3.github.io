3929bcfccf1eb13e6ab022fd41778fbe829be853
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-24 17:29:17 +0900

    Use flowbite-react in dry hood table

--- src/components/cats/DryFoods.tsx
+++ src/components/cats/DryFoods.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 
 import { DryFoods, Food } from "../../domains/Cat.ts";
+import { Table } from "flowbite-react";
 
-const DryFoodTableBody: React.FC<{ feed: Food; der: number }> = (
-  props,
-) => {
+const DryFoodTableBody: React.FC<{ feed: Food; der: number }> = (props) => {
   return (
-    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-      <td className="px-6 py-4">
+    <Table.Row>
+      <Table.Cell className="px-6 py-4">
         <a
           className="hover:underline text-blue-600"
           href={props.feed.url}
@@ -15,18 +14,26 @@ const DryFoodTableBody: React.FC<{ feed: Food; der: number }> = (
         >
           {props.feed.name}
         </a>
-      </td>
-      <td className="px-6 py-4">{props.feed.kcalPer100}</td>
-      <td className="px-6 py-4">
-        {Math.round(props.der / props.feed.kcalPer100 * 100)}
-      </td>
-      <td className="px-6 py-4">{props.feed.nutrition.carbohydrate}</td>
-      <td className="px-6 py-4">{props.feed.nutrition.protein}</td>
-      <td className="px-6 py-4">{props.feed.nutrition.fat}</td>
-      <td className="px-6 py-4">{props.feed.nutrition.fiber}</td>
-      <td className="px-6 py-4">{props.feed.nutrition.ash}</td>
-      <td className="px-6 py-4">{props.feed.nutrition.moisture}</td>
-    </tr>
+      </Table.Cell>
+      <Table.Cell className="px-6 py-4">{props.feed.kcalPer100}</Table.Cell>
+      <Table.Cell className="px-6 py-4">
+        {Math.round((props.der / props.feed.kcalPer100) * 100)}
+      </Table.Cell>
+      <Table.Cell className="px-6 py-4">
+        {props.feed.nutrition.carbohydrate}
+      </Table.Cell>
+      <Table.Cell className="px-6 py-4">
+        {props.feed.nutrition.protein}
+      </Table.Cell>
+      <Table.Cell className="px-6 py-4">{props.feed.nutrition.fat}</Table.Cell>
+      <Table.Cell className="px-6 py-4">
+        {props.feed.nutrition.fiber}
+      </Table.Cell>
+      <Table.Cell className="px-6 py-4">{props.feed.nutrition.ash}</Table.Cell>
+      <Table.Cell className="px-6 py-4">
+        {props.feed.nutrition.moisture}
+      </Table.Cell>
+    </Table.Row>
   );
 };
 
@@ -34,26 +41,26 @@ const DryFoodTable: React.FC<{ der: number }> = (props: { der: number }) => {
   return (
     <div className="relative overflow-x-auto">
       <h1 className="text-2xl font-bold my-4 ">ドライフード</h1>
-      <table className="w-full text-sm text-left">
-        <thead>
-          <tr>
-            <th className="px-6 py-3">名称</th>
-            <th className="px-6 py-3">カロリー(kcal/100g)</th>
-            <th className="px-6 py-3">必要グラム数</th>
-            <th className="px-6 py-3">糖質(概算)</th>
-            <th className="px-6 py-3">たんぱく質</th>
-            <th className="px-6 py-3">脂質</th>
-            <th className="px-6 py-3">粗繊維</th>
-            <th className="px-6 py-3">灰分</th>
-            <th className="px-6 py-3">水分</th>
-          </tr>
-        </thead>
-        <tbody>
+      <Table>
+        <Table.Head>
+          <Table.HeadCell className="px-6 py-3">名称</Table.HeadCell>
+          <Table.HeadCell className="px-6 py-3">
+            カロリー(kcal/100g)
+          </Table.HeadCell>
+          <Table.HeadCell className="px-6 py-3">必要グラム数</Table.HeadCell>
+          <Table.HeadCell className="px-6 py-3">糖質(概算)</Table.HeadCell>
+          <Table.HeadCell className="px-6 py-3">たんぱく質</Table.HeadCell>
+          <Table.HeadCell className="px-6 py-3">脂質</Table.HeadCell>
+          <Table.HeadCell className="px-6 py-3">粗繊維</Table.HeadCell>
+          <Table.HeadCell className="px-6 py-3">灰分</Table.HeadCell>
+          <Table.HeadCell className="px-6 py-3">水分</Table.HeadCell>
+        </Table.Head>
+        <Table.Body>
           {DryFoods.map((f) => (
             <DryFoodTableBody key={f.id} der={props.der} feed={f} />
           ))}
-        </tbody>
-      </table>
+        </Table.Body>
+      </Table>
     </div>
   );
 };
