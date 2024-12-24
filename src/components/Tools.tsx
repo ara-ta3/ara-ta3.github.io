30e844283324e6377c9e63a41f5b0828ca97001f
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-24 20:13:58 +0900

    Use card in home

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,3 +1,4 @@
+import { Card } from "flowbite-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -13,24 +14,21 @@ const Tools: React.FC = () => {
     {
       id: 2,
       name: "猫のカロリー計算",
-      description: "RER/DERの計算をします。",
+      description:
+        "猫の体重と活動レベルからRER/DERとごはんの必要な量を計算します。",
       navigate: () => navigate("/cat/calorie"),
     },
   ];
   return (
     <section id="tools" className="py-12 bg-gray-50">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-6 text-center">Tools</h2>
+        <h2 className="text-3xl font-bold mb-4 text-center">Tools</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {tools.map((tool) => (
-            <div
-              key={tool.id}
-              className="bg-white shadow-md rounded-lg p-6 cursor-pointer hover:bg-gray-100 transition"
-              onClick={tool.navigate}
-            >
-              <h3 className="text-xl font-semibold mb-2">{tool.name}</h3>
-              <p className="text-gray-600">{tool.description}</p>
-            </div>
+            <Card key={tool.id} className="w-full" onClick={tool.navigate}>
+              <h5 className="text-2xl font-bold text-gray-900">{tool.name}</h5>
+              <p className="font-normal text-gray-700">{tool.description}</p>
+            </Card>
           ))}
         </div>
       </div>
