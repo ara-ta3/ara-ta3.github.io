af9124fc2f4221d82f5b1fb8548e28df880a227c
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-28 10:39:31 +0900

    Move schedules to pages

--- src/pages/schedules/+Page.tsx
+++ src/pages/schedules/+Page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Header from "../components/Header";
-import { HourlyPieChart } from "../components/Schedule";
+import Header from "../../components/Header";
+import { HourlyPieChart } from "../../components/Schedule";
 
 const innerData = [
   { id: "睡眠", label: "睡眠", value: 7.5 },
