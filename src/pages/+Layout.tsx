00aafab37eee4842cc5fd91961ca6f8e0b2739cf
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-28 17:30:23 +0900

    Add gtm

--- src/pages/+Layout.tsx
+++ src/pages/+Layout.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import "../index.css";
 
 function Layout({ children }): React.FC {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtm.js?id=GTM-W5LM9X33";
+    document.head.appendChild(script);
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "gtm.js",
+      "gtm.start": new Date().getTime(),
+    });
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
   return (
     <>
       <Header />
