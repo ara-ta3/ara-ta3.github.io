265f75fd225da1a4bc93ffe1de0577cfbda3c4ab
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-31 13:53:50 +0900

    Add cat calorie link to menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "flowbite-react";
+import { MegaMenu, Navbar } from "flowbite-react";
 import React from "react";
 
 const Header: React.FC = () => {
@@ -7,7 +7,20 @@ const Header: React.FC = () => {
       <Navbar.Brand href="/">ara-ta3の物置</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="/#tools">Tools</Navbar.Link>
+        <MegaMenu.Dropdown toggle={<>Tools</>}>
+          <ul className="grid grid-cols-3">
+            <div className="space-y-4 p-4">
+              <li>
+                <a
+                  href="/cat/calorie"
+                  className="hover:text-primary-600 dark:hover:text-primary-500"
+                >
+                  猫のカロリー計算
+                </a>
+              </li>
+            </div>
+          </ul>
+        </MegaMenu.Dropdown>
         <Navbar.Link href="https://github.com/ara-ta3/ara-ta3.github.io">
           GitHub
         </Navbar.Link>
