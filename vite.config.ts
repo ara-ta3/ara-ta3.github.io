80d13f6fba33db8c46735849967a28aef79894bc
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-04-03 12:58:45 +0900

    Add plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), flowbiteReact()],
   ssr: {
     noExternal: [/^d3.*$/],
   },
