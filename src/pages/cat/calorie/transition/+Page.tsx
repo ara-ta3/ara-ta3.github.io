645207de9420a52aab8c19b15571a1c489ea4d86
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-05-13 01:50:48 +0900

    Add

--- src/pages/cat/calorie/transition/+Page.tsx
+++ src/pages/cat/calorie/transition/+Page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { CalorieTabs } from "../../../../components/cats/CalorieTabs";
 
 const CatCalorieTransition: React.FC = () => {
-  return <CalorieTabs defaultTab="フード切り替えプラン" />;
+  return <CalorieTabs defaultTab="フード切り替えプラン" initialFoodId={null} />;
 };
 
 export default CatCalorieTransition;
