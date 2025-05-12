259bb8b28f2f26645d39aa2bdcb5ee50f73bfe64
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-05-13 01:33:38 +0900

    Add query params

--- src/pages/cat/calorie/+Page.tsx
+++ src/pages/cat/calorie/+Page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { CalorieTabs } from "../../../components/cats/CalorieTabs";
+import { usePageContext } from "vike-react/usePageContext";
 
 const CatCalorie: React.FC = () => {
-  return <CalorieTabs defaultTab="計算" />;
+  const pageContext = usePageContext();
+  const foodId = pageContext.urlParsed.search["foodId"];
+
+  return <CalorieTabs defaultTab="計算" initialFoodId={foodId} />;
 };
 
 export default CatCalorie;
