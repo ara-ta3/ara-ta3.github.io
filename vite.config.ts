ed4d8d48936dc50048052773c1a868a3643d2b41
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-05-18 21:52:53 +0900

    Add vites/tconfig and @ import

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import "vitest/config";
 
 export default defineConfig({
   base: "/",
   plugins: [react()],
   ssr: {
     noExternal: [/^d3.*$/],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   test: {
     globals: true,
