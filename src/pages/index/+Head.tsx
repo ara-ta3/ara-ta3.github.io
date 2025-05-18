6472c3957cad8bfeceeb91c4543501f789b4d507
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-05-18 17:47:31 +0900

    Modify

--- src/pages/index/+Head.tsx
+++ src/pages/index/+Head.tsx
@@ -3,6 +3,7 @@ import React from "react";
 const Head: React.FC = () => {
   return (
     <>
+      <title>ara-ta3の個人ページ</title>
       <link rel="canonical" href="https://ara-ta3.github.io/" />
     </>
   );
