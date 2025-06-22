d47e26159a735c47d4b80550970cd6c7289ce3b5
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-06-22 20:39:26 +0900

    Fix design in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,37 +1,33 @@
 import React from "react";
 import Logo from "@/assets/images/logo.png";
 
+const HeaderMenuItem: React.FC<{ href: string; children: string }> = ({
+  href,
+  children,
+}) => {
+  return (
+    <a
+      className="text-primary-500 hover:text-primary-700 text-sm font-medium leading-normal"
+      href={href}
+    >
+      {children}
+    </a>
+  );
+};
+
 const Header: React.FC = () => {
   return (
-    <header className="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#f1f2f4] px-10 py-3">
-      <div className="flex items-center gap-4 text-[#121416]">
-        <div className="size-4">
-          <img src={Logo} alt="Logo" />
+    <header className="flex items-center justify-between border-b border-solid border-b px-8 py-4 ">
+      <div className="flex items-center gap-4 ">
+        <div>
+          <img src={Logo} alt="Logo" className="w-24" />
         </div>
-        <h2 className="text-[#121416] text-lg font-bold leading-tight tracking-[-0.015em]">
-          ara-ta3のポートフォリオ
-        </h2>
       </div>
       <div className="flex flex-1 justify-end gap-8">
         <div className="flex items-center gap-9">
-          <a
-            className="text-[#121416] text-sm font-medium leading-normal"
-            href="#projects"
-          >
-            プロジェクト
-          </a>
-          <a
-            className="text-[#121416] text-sm font-medium leading-normal"
-            href="#articles"
-          >
-            記事
-          </a>
-          <a
-            className="text-[#121416] text-sm font-medium leading-normal"
-            href="#contact"
-          >
-            SNSアカウント
-          </a>
+          <HeaderMenuItem href="#project">プロジェクト</HeaderMenuItem>
+          <HeaderMenuItem href="#articles">記事</HeaderMenuItem>
+          <HeaderMenuItem href="#contact">SNSアカウント</HeaderMenuItem>
         </div>
       </div>
     </header>
