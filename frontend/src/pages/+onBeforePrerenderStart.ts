0ff203ee651d3af8bbbabe261e3c608d8b40f5d3
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-06-23 22:22:28 +0900

    Fix

--- frontend/src/pages/+onBeforePrerenderStart.ts
+++ frontend/src/pages/+onBeforePrerenderStart.ts
@@ -3,7 +3,5 @@ import { getAllProjectIds } from "@/utils/projects";
 export default function onBeforePrerenderStart() {
   const projectIds = getAllProjectIds();
   const projectUrls = projectIds.map((id) => `/projects/${id}`);
-
-  // 重複を排除
-  return [...new Set(projectUrls)];
+  return projectUrls;
 }
